625b67a937caa1791b9db5fa7f22362c2f4d6783
Celeste A <57380757+Celgurl@users.noreply.github.com>
2019-11-10 20:20:18 +1100

    5th commit
    got localStorage to work and starting laying out review page (sign_up3.html)

---
+++ script.js
@@ -0,0 +1,15 @@
+    function subSelect1() {
+        document.getElementById("sub1").style.transform = "scale(1.25)";
+        document.getElementById("sub2").style.transform = "scale(1)"; 
+        document.getElementById("sub3").style.transform = "scale(1)";
+            }
+    function subSelect2() {
+        document.getElementById("sub2").style.transform = "scale(1.25)";
+        document.getElementById("sub3").style.transform = "scale(1)"; 
+        document.getElementById("sub1").style.transform = "scale(1)";
+            }
+    function subSelect3() {
+        document.getElementById("sub3").style.transform = "scale(1.25)"; 
+        document.getElementById("sub2").style.transform = "scale(1)"; 
+        document.getElementById("sub1").style.transform = "scale(1)"; 
+            }
